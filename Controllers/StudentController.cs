c0261612620f0ce4f2b4b494c6af513f4b4d9a33
suchismita senapati <suchismita.senapati@bipros.com>
2021-07-08 17:55:18 +0530

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class StudentController : ApiController
+    {
+        IList<Student> studentList = new List<Student>()
+        {
+            new Student()
+            {
+                Id = 1,
+                CourseId = 1,
+                UserName= "Nil",
+                Age= 30
+            },
+            new Student()
+            {
+                Id = 2,
+                CourseId = 2,
+                UserName= "Satya",
+                Age= 22
+            },
+            new Student()
+            {
+                Id = 3,
+                CourseId = 1,
+                UserName= "Deboshree",
+                Age= 27
+            }
+        };
+
+        public IList<Student> GetAllStudent()
+        {
+            return studentList;
+        }
+
+        public IList<Student> GetById(long id)
+        {
+            IList<Student> studentListByCourseId = studentList.Where(st => st.CourseId == id).ToList();
+            if (studentListByCourseId.Count > 0)
+            {
+                return studentListByCourseId;
+            }
+            else
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+        }
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Student
+    {
+        [Key]
+        public long Id { get; set; }
+        public long CourseId { get; set; }
+        public string UserName { get; set; }
+        public long Age { get; set; }
+    }
+}
